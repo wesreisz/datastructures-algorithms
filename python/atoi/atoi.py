e8051c773897c03e24cd55b755ef65c82b67bd46
Wesley Reisz <wes@wesleyreisz.com>
2018-12-03 10:43:29 -0500

    Implemented the convert method in python
    Approach replaces numbers with characters and then converts to an
    integer

--- python/atoi/atoi.py
+++ python/atoi/atoi.py
@@ -1,2 +1,11 @@
+from string import ascii_lowercase
+LETTERS = {letter: str(index)
+           for index, letter in enumerate(ascii_lowercase, start=1)}
+
+
 def atoi(text):
-    return 123
+    text = text.lower()
+    numbers = [LETTERS[character]
+               for character in text if character in LETTERS]
+    val = ''.join(numbers)
+    return int(val)
